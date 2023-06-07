2a16e999dfdb9a855a4e9b40ed3f46b4bb8e014b
user <wpelletier.development@yahoo.com>
2023-06-07 15:27:42 -0400

    setup home page for display video box

--- client/src/component/videobox.js
+++ client/src/component/videobox.js
@@ -4,22 +4,31 @@ import React, {useState} from "react";
 import "../style/videobox.css";
 import nothumb from "../assets/thumbnail.png";
 
+const MAX_LENGTH_TITLE = 23;
 
 function VideoBox({thumbnail,title,channel}) {
 
    
+    var ctitle = ""
 
+    if (title.length >= MAX_LENGTH_TITLE) {
+
+        ctitle = title.substring(0,MAX_LENGTH_TITLE + 1) + "...";
+    } else {
+
+        ctitle = title
+    }
 
     return (
-        <div href="/home" className="box">
+        <a href="/home" className="box">
         
            
             <img className="thumbnail" src={thumbnail} alt="video thumbnail"/>
-            <a href="/home" className="title">{title}</a>
-            <a href="/channel" className="channel">{channel}</a>
+            <p href="/home" className="title">{ctitle}</p>
+            <p href="/channel" className="channel">{channel}</p>
                 
             
-        </div>
+        </a>
 
     );
 
