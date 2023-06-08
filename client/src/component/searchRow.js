5957a422ac5b6759db121b2bb22a336940dda818
user <wpelletier.development@yahoo.com>
2023-06-08 13:18:22 -0400

    tweak a little the style of the search page

--- client/src/component/searchRow.js
+++ client/src/component/searchRow.js
@@ -16,12 +16,14 @@ function SearchRow({type,title,channel,isrc}) {
         <div>
             {(type === "video" && 
                 <div className="row">
-                    <img className="search-vthumbnail" src={`http://localhost:3000/thumbnail/${isrc}`} alt="thumbnail or channel"/>
-                    <div className="search-info">
-                        <p className="search-title">{title}</p>
-                        <p className="search-channel">{channel}</p>
-                    </div>
-                    <div className="spacer"></div>
+                   
+                        <img className="video-thumbnail" src={`http://localhost:3000/thumbnail/${isrc}`} alt="thumbnail or channel"/>
+                        <div className="search-info">
+                            <a href={`/video/${isrc}`} className="search-title">{title}</a>
+                            <br/>
+                            <a href={`/channel/${channel}`} className="search-channel">{channel}</a>
+                        </div>
+                    
                     <div className="far-left">
 
                         <p className="duration">1:00</p>
@@ -34,13 +36,14 @@ function SearchRow({type,title,channel,isrc}) {
             {(type === "channel" && 
                 <div className="row">
                     
-                    {(isrc != null) && <img className="search-cthumbnail" src={`http://localhost:3000/thumbnail/${isrc}`} alt="thumbnail or channel"/>}
-                    {(isrc == null) && <img className="search-cthumbnail" src={dChannelImg} alt="thumbnail or channel"/>}
-                    <div className="search-info">
-                        <p className="search-title">{channel}</p>
+                        {(isrc != null) && <img className="channel-thumbnail" src={`http://localhost:3000/thumbnail/${isrc}`} alt="thumbnail or channel"/>}
+                        {(isrc == null) && <img className="channel-thumbnail" src={dChannelImg} alt="thumbnail or channel"/>}
+                    
+                    <div className="search-cinfo">
+                        <a href={`/channel/${channel}`} className="search-title">{channel}</a>
                     
                     </div>
-                    <div className="spacer"></div>
+                   
                     
                 </div>
         
